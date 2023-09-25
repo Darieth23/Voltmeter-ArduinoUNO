19cba3e5c4285a51d34d137de7bc6e2bcc2c1cf0
Xuthyst <darieth.fonseca@ucr.ac.cr>
2023-09-25 00:39:43 -0600

    actualizacion

--- src/voltmeter/voltmeter.ino
+++ src/voltmeter/voltmeter.ino
@@ -4,26 +4,35 @@
 
 //Pines analógicos de entradas de tensión
 int volt1 = A0;
+int switchDC_AC = A5;
+
+//Signos de las señales DC
+int sign1 = 2;
 
 //leds
 int led_one = 13;
 
 //Variables de almacenar tensiones:
 float v1 = 0;
+float valSign1 = 0;
+float valSwitchDC_AC = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   //Pines de tensión
   pinMode(volt1, INPUT);
+  pinMode(sign1, INPUT);
+  pinMode(switchDC_AC, INPUT);
   // Pines de los leds
   pinMode(led_one, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  v1 = 0;
-  v1 = analogRead(volt1);
+  v1 = analogRead(volt1)* (5.0 / 1023.0);
+  valSign1 = digitalRead(sign1);
+  valSwitchDC_AC = digitalRead(switchDC_AC);
   //if (v1 > 0){
   //  digitalWrite(led_one, HIGH);
   //  delay(5000);
@@ -34,11 +43,24 @@ void loop() {
   //}
   int brightness = map(v1, 0, 1023, 0, 255);
   digitalWrite(led_one, brightness);
+  Serial.print("Valor del switch: ");
+  Serial.print(valSwitchDC_AC);
+  
+
+  if(valSwitchDC_AC == 1){
+    Serial.print("Signo positivo de la señal DC: ");
+    Serial.print(valSign1);
+  }
+  else{
+    Serial.print("Signo negativo de la señal DC: ");
+    Serial.print(valSign1);
+
+  }
   // Print the sensor value and LED brightness to the serial monitor
   Serial.print("Sensor Value: ");
   Serial.print(v1);
-  Serial.print(" | LED Brightness: ");
-  Serial.println(brightness);
+  //Serial.print(" | LED Brightness: ");
+  //Serial.println(brightness);
 
   delay(100);
 }
